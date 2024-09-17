8a8cc312806203ed8c49c2b9fc81e3a3a534594a
nitramlam <martinlamalle@gmail.com>
2024-09-17 13:22:15 +0200

    Correction route GET /

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
